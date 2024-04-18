f804e558f006fedec0059fc821671a48cf67f354
Daniel Felipe Montenegro <36195171+dafmontenegro@users.noreply.github.com>
2024-04-18 00:33:33 -0500

    class LEDRGB, class ObjectDetector, class Camera, class RealTimeObjectDetection, class StorageManager

--- RealTimeObjectDetection/remote_camera_object_detection_mk2.py
+++ RealTimeObjectDetection/remote_camera_object_detection_mk2.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import time
 import shutil
+import logging
 import argparse
 import threading
 import RPi.GPIO as GPIO
@@ -10,144 +11,184 @@ from tflite_support.task import core
 from tflite_support.task import vision
 from tflite_support.task import processor
 
-red_led = 33
-green_led = 35
-blue_led = 37
+class LEDRGB:
+    colors = {
+        "red": (1, 0, 0),
+        "green": (0, 1, 0),
+        "blue": (0, 0, 1),
+        "yellow": (1, 1, 0),
+        "magenta": (1, 0, 1),
+        "cyan": (0, 1, 1),
+        "white": (1, 1, 1),
+        "off": (0, 0, 0)
+    }
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(red_led, GPIO.OUT)
-GPIO.setup(green_led, GPIO.OUT)
-GPIO.setup(blue_led, GPIO.OUT)
+    def __init__(self, red_led=33, green_led=35, blue_led=37):
+        GPIO.setmode(GPIO.BOARD)
+        self.red_led = red_led
+        self.green_led = green_led
+        self.blue_led = blue_led
+        GPIO.setup(self.red_led, GPIO.OUT)
+        GPIO.setup(self.green_led, GPIO.OUT)
+        GPIO.setup(self.blue_led, GPIO.OUT)
+    
+    def _set_color(self, color_name):
+        color = self.colors.get(color_name.lower(), self.colors["off"])
+        GPIO.output(self.red_led, color[0])
+        GPIO.output(self.green_led, color[1])
+        GPIO.output(self.blue_led, color[2])
 
-colors = {
-    "red": (1, 0, 0),
-    "green": (0, 1, 0),
-    "blue": (0, 0, 1),
-    "yellow": (1, 1, 0),
-    "magenta": (1, 0, 1),
-    "cyan": (0, 1, 1),
-    "white": (1, 1, 1),
-    "off": (0, 0, 0)
-}
+    def __getattr__(self, color_name):
+        return lambda: self._set_color(color_name.lower())
 
-def led_rgb(color):
-    color = colors[color]
-    GPIO.output(red_led, color[0])
-    GPIO.output(green_led, color[1])
-    GPIO.output(blue_led, color[2])
+class ObjectDetector:
+    def __init__(self, model_name="efficientdet_lite0.tflite", num_threads=4, score_threshold=0.5, max_results=1, category_name_allowlist=["person"]):
+        base_options = core.BaseOptions(file_name=model_name, use_coral=False, num_threads=num_threads)
+        detection_options = processor.DetectionOptions(max_results=max_results, score_threshold=score_threshold, category_name_allowlist=category_name_allowlist)
+        options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
+        self.detector = vision.ObjectDetector.create_from_options(options)
 
-events = 0
-event_frames = []
-event_path = None
-last_detection = None
-app = Flask(__name__)
-score_threshold = 0.5
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-base_options = core.BaseOptions(file_name="efficientdet_lite0.tflite", use_coral=False, num_threads=4)
-detection_options = processor.DetectionOptions(max_results=1, score_threshold=score_threshold, category_name_allowlist=["person"])
-options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
-detector = vision.ObjectDetector.create_from_options(options)
-_, last_frame = cap.read()
-
-def visualize(image, detection_result):
-    global events
-    global event_path
-    global event_frames
-    global last_detection
-    time_localtime = time.localtime()
-    for detection in detection_result.detections:
-        bbox = detection.bounding_box
-        start_point = bbox.origin_x, bbox.origin_y
-        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-        cv2.rectangle(image, start_point, end_point, (0, 0, 255), 3)
-        category_name = detection.categories[0].category_name
-        text_location = (7 + bbox.origin_x, 21 + bbox.origin_y)
-        cv2.putText(image, category_name, text_location, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    cv2.putText(image, time.strftime("%B%d/%Y %H:%M:%S", time_localtime), (21, 42), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    if detection_result.detections:
-        if not event_frames:
-            hour, mins, day = time.strftime("%Hhr_%Mmin%Ssec_%B%d", time_localtime).split("_")
-            event_path = os.path.join("events", day, hour, f"{hour}{mins}{day}.mp4")
-        last_detection = time.time()
-        event_frames.append(image)
-    else:
-        if last_detection and ((time.time() - last_detection) >= 30):
-            if len(event_frames) >= 24:
-                events += 1
-                os.makedirs(os.path.dirname(event_path), exist_ok=True)
-                out = cv2.VideoWriter(event_path, cv2.VideoWriter_fourcc(*"mp4v"), 24, (1280, 720))
-                print(f"SAVE EVENT #{events} ({int(len(event_frames)/24)} secs): {event_path}")
-                for event_frame in event_frames:
-                    out.write(event_frame)
-                out.release()
-            last_detection = None
-            event_path = None
-            event_frames = []
-            led_rgb("blue")
-    return image
-
-def main():
-    led_rgb("blue")
-    global last_frame
-    global event_frames
-    while cap.isOpened():
-        _, image = cap.read()
+    def detections(self, image):
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        detection_result = detector.detect(vision.TensorImage.create_from_array(rgb_image))
-        last_frame = visualize(image, detection_result)
-        if len(event_frames) >= 24:
-            led_rgb("red")
-    cap.release()
+        return self.detector.detect(vision.TensorImage.create_from_array(rgb_image)).detections
 
-def generate_frames():
-    while cap.isOpened():
-        frame = cv2.imencode(".jpg", last_frame)[1].tobytes()
+class Camera:
+    def __init__(self, frame_width=1280, frame_height=720, camera_number=0):
+        self.video_capture = cv2.VideoCapture(camera_number)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+    def frame(self):
+        _, frame = self.video_capture.read()
+        return frame
+
+class RealTimeObjectDetection:
+    def __init__(self, frame_width=1280, frame_height=720, camera_number=0, model_name="efficientdet_lite0.tflite", num_threads=4, score_threshold=0.5, 
+                 max_results=1, category_name_allowlist=["person"], folder_name="events", storage_capacity=21):
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        self.led_rgb = LEDRGB(33, 35, 37)
+        self.camera = Camera(frame_width, frame_height, camera_number)
+        self.object_detector = ObjectDetector(model_name, num_threads, score_threshold, max_results, category_name_allowlist)
+        self.storage_manager = StorageManager(folder_name, storage_capacity)
+        self.storage_manager.supervise_folder_capacity()
+        self.last_detection_timestamp = None
+        self.frame = self.camera.frame()
+        self.folder_name = folder_name
+        self.output_path = None
+        self.frame_buffer = []
+        self.events = 0
+
+    def guard(self, fps=24, max_detection_delay=30, event_check_interval=50):
+        self.led_rgb.blue()
+        while self.isOpened():
+            detections, time_localtime = self.process_frame((0, 0, 255), 1, 2, cv2.FONT_HERSHEY_SIMPLEX)
+            if detections:
+                if not self.frame_buffer:
+                    hour, mins, day = time.strftime("%Hhr_%Mmin%Ssec_%B%d", time_localtime).split("_")
+                    self.output_path = os.path.join(self.folder_name, day, hour, f"{hour}{mins}{day}.mp4")
+                self.last_detection_timestamp = time.time()
+                self.frame_buffer.append(self.frame)
+            else:
+                if self.last_detection_timestamp and ((time.time() - self.last_detection_timestamp) >= max_detection_delay):
+                    if len(self.frame_buffer) >= fps:
+                        self.events += 1
+                        os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
+                        out = cv2.VideoWriter(self.output_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (self.frame_width, self.frame_height))
+                        print(f"SAVE EVENT #{self.events}: {int(len(self.frame_buffer)/24)} seconds {self.output_path}")
+                        for frame in self.frame_buffer:
+                            out.write(frame)
+                        out.release()
+                        if self.events >= event_check_interval:
+                            self.storage_manager.supervise_folder_capacity()
+                    self.last_detection_timestamp = None
+                    self.output_path = None
+                    self.frame_buffer = []
+                    self.led_rgb.blue()
+            if len(self.frame_buffer) >= fps:
+                self.led_rgb.red()
+            if cv2.waitKey(1) == 27:
+                self.close()
+
+    def process_frame(self, color=(0, 0, 255), font_size=1, font_thickness=2, font=cv2.FONT_HERSHEY_SIMPLEX):
+        frame = self.camera.frame()
+        time_localtime = time.localtime()
+        detections = self.object_detector.detections(frame)
+        for detection in detections:
+            box = detection.bounding_box
+            start_point = box.origin_x, box.origin_y
+            end_point = box.origin_x+box.width, box.origin_y+box.height
+            category_name = detection.categories[0].category_name
+            text_position = (7+box.origin_x, 21+box.origin_y)
+            cv2.rectangle(frame, start_point, end_point, color, font_thickness)
+            cv2.putText(frame, category_name, text_position, font, font_size, color, font_thickness)
+        cv2.putText(frame, time.strftime("%B%d/%Y %H:%M:%S", time_localtime), (21, 42), cv2.FONT_HERSHEY_SIMPLEX, font_size, color, font_thickness)
+        self.frame = frame
+        return detections, time_localtime
+
+    def isOpened(self):
+        return self.camera.video_capture.isOpened()
+    
+    def close(self):
+        self.camera.video_capture.release()
+
+class StorageManager:
+    def __init__(self, events_folder="events", storage_capacity=21):
+        self.events_folder = events_folder
+        self.storage_capacity = storage_capacity
+
+    @staticmethod
+    def folder_size_gb(folder_path):
+        total_size_bytes = 0
+        for dirpath, _, filenames in os.walk(folder_path):
+            for filename in filenames:
+                file_path = os.path.join(dirpath, filename)
+                total_size_bytes += os.path.getsize(file_path)
+        return total_size_bytes / (1024 ** 3)
+    
+    @staticmethod
+    def delete_folder(folder_path):
+        folder_size = StorageManager.folder_size_gb(folder_path)
+        shutil.rmtree(folder_path)
+        print(f"STORAGE: '{folder_path}' was deleted (-{folder_size:.4f} GB)")
+        return folder_size
+
+    def supervise_folder_capacity(self):
+        events_folder_size = StorageManager.folder_size_gb(self.events_folder)
+        print(f"STORAGE: '{self.events_folder}' is ({events_folder_size:.4f} GB)")
+        while events_folder_size > self.storage_capacity:
+            folder_to_delete = os.path.join(self.events_folder, min(os.listdir(self.events_folder)))
+            events_folder_size -= StorageManager.delete_folder(folder_to_delete)
+
+app = Flask(__name__)
+folder_name = "events"
+remote_camera = RealTimeObjectDetection(1280, 720, 0, "efficientdet_lite0.tflite", 4, 0.5, 3, ["person", "dog", "cat"], folder_name, 21)
+
+def real_time_transmission():
+    while remote_camera.isOpened():
+        frame = cv2.imencode(".jpg", remote_camera.frame)[1].tobytes()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
-def get_folder_size(folder_path):
-    total_size = 0
-    for dirpath, _, filenames in os.walk(folder_path):
-        for filename in filenames:
-            file_path = os.path.join(dirpath, filename)
-            total_size += os.path.getsize(file_path)
-    return total_size / 1024 / 1024 / 1024
-
-def delete_oldest_folder(folder_path):
-    oldest_folder = min(os.listdir(folder_path))
-    folder_to_delete = os.path.join(folder_path, oldest_folder)
-    folder_deleted_size = get_folder_size(folder_to_delete)
-    shutil.rmtree(folder_to_delete)
-    print(f"STORAGE: '{folder_to_delete}' was deleted (-{folder_deleted_size:.4f} GB)")
-    return folder_deleted_size
-
-def folder_verification_thread(events_folder):
-    while cap.isOpened():
-        events_folder_size = get_folder_size(events_folder)
-        print(f"STORAGE: '{events_folder}' is ({events_folder_size:.4f} GB)")
-        while events_folder_size > 21:
-            events_folder_size -= delete_oldest_folder(events_folder)
-        time.sleep(1800)
-
 @app.route("/")
-def video_feed():
-    return Response(generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--reset-events", action="store_true", help="Reset events folder")
-args = parser.parse_args()
+def stream_video():
+    return Response(real_time_transmission(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset-events", action="store_true", help="Reset events folder")
+    args = parser.parse_args()
     try:
-        main_thread = threading.Thread(target=main)
-        main_thread.start()
         if args.reset_events:
             shutil.rmtree("events")
             print(f"STORAGE: 'events' was deleted")
         os.makedirs("events", exist_ok=True)
-        folder_thread = threading.Thread(target=folder_verification_thread, args=("events",))
-        folder_thread.start()
-        app.run(host="0.0.0.0", port=80, threaded=True)
+        flask_thread = threading.Thread(target=app.run, kwargs={"host": "0.0.0.0", "port": 80, "threaded": True})
+        flask_thread.start()
+        remote_camera.guard(24, 30, 50)
+    except Exception as e:
+        print(f"ERROR: {e}")
+        remote_camera.close()
+        GPIO.cleanup()
     finally:
+        remote_camera.close()
         GPIO.cleanup()
